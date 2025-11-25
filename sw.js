3e029eadae79a632b84e7c5e52452b1cda310343
remodelproutah-eng <remodelproutah@gmail.com>
2025-11-24 19:44:01 -0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,31 @@
+// Very simple offline cache for the app shell
+const CACHE_NAME = 'remodelpro-invoice-v1';
+const ASSETS = [
+  'invoice.html',
+  'logo.png',
+  'icon-192.png',
+  'icon-512.png',
+  'manifest.webmanifest'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => key !== CACHE_NAME && caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
